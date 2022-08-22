9d277ae16e3cb4f49c59db3c0bf41107e7ef7672
Dmitry <Logan6338@mail.ru>
2022-08-22 19:03:33 +0300

    Задача 3

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.WriteLine("Введите размер массива");
+﻿/*Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+[3 7 22 2 78] -> 76*/
+
+Console.WriteLine("Введите размер массива");
 int size = Convert.ToInt32(Console.ReadLine());
 double[] numbers = new double[size];
 FillArrayRandomNumbers(numbers);
