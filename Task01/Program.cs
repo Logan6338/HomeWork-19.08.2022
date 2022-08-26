efcedf37fa7c89b1094b7e15754052199c10b68c
Dmitry <Logan6338@mail.ru>
2022-08-26 21:00:07 +0300

    Задача 1

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -9,11 +9,16 @@ Console.WriteLine("Вот наш массив: ");
 PrintArray(numbers);
 int count = 0;
 
+void FindEvenCount(int[] numbers)
+{
 for (int z = 0; z < numbers.Length; z++)
 if (numbers[z] % 2 == 0)
 count++;
 
 Console.WriteLine($"всего {numbers.Length} чисел, {count} из них чётные");
+
+}
+FindEvenCount(numbers);
 
 void FillArrayRandomNumbers(int[] numbers)
 {
