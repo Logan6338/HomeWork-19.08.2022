87c47de57be94e541c5c18617b6a068d212a8898
Dmitry <Logan6338@mail.ru>
2022-08-28 23:58:19 +0300

    Задача 3

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -10,6 +10,24 @@ PrintArray(numbers);
 double min = Int32.MaxValue;
 double max = Int32.MinValue;
 
+void FillArrayRandomNumbers(double[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+        }
+}
+void PrintArray(double[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
 void FindMaxMinDifference(double[] numbers)
 {
 for (int z = 0; z < numbers.Length; z++)
@@ -28,21 +46,3 @@ Console.WriteLine($"всего {numbers.Length} чисел. Максимальн
 Console.WriteLine($"Разница между максимальным и минимальным значением = {max - min}");
 }
 FindMaxMinDifference(numbers);
-
-void FillArrayRandomNumbers(double[] numbers)
-{
-    for(int i = 0; i < numbers.Length; i++)
-        {
-            numbers[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
-        }
-}
-void PrintArray(double[] numbers)
-{
-    Console.Write("[ ");
-    for(int i = 0; i < numbers.Length; i++)
-        {
-            Console.Write(numbers[i] + " ");
-        }
-    Console.Write("]");
-    Console.WriteLine();
-}
